643319c38b99385b84adcf7d94614f71aa151bc2
terrence.tang <terrence.tang@vivotek.com>
2024-06-14 09:44:20 +0800

    update

---
+++ add_source.sh
@@ -0,0 +1,18 @@
+git submodule add git@github.com:mehdihadeli/awesome-go-education.git source/awesome-go-education
+# 340 start
+git submodule add git@github.com:PacktPublishing/Domain-Driven-Design-with-GoLang.git source/Domain-Driven-Design-with-GoLang
+# 234 start
+git submodule add git@github.com:evrone/go-clean-template.git source/go-clean-template
+# 6k start
+git submodule add git@github.com:sesigl/go-project-ddd-template.git source/go-project-ddd-template
+# 43 start
+git submodule add git@github.com:sklinkert/go-ddd.git source/go-ddd
+# 104 start
+git submodule add git@github.com:ThreeDotsLabs/wild-workouts-go-ddd-example.git source/wild-workouts-go-ddd-example
+# 5k start
+git submodule add git@github.com:mehdihadeli/awesome-go-education.git source/awesome-go-education2
+# 340 start
+git submodule add git@github.com:bxcodec/go-clean-arch.git source/go-clean-arch
+# 8.8k start
+git submodule add git@github.com:marcusolsson/goddd.git source/goddd
+# 2.4k start
